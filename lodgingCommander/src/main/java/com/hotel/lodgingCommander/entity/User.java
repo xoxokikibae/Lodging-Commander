8e7f09379f3fdf3b121df6a294dccf1161da0a01
xoxokikibae <xoxokikibae@gmail.com>
2024-08-13 16:08:15 +0900

    git merge 1

--- lodgingCommander/src/main/java/com/hotel/lodgingCommander/entity/User.java
+++ lodgingCommander/src/main/java/com/hotel/lodgingCommander/entity/User.java
@@ -43,7 +43,7 @@ public class User {
     private String grade;
 
     @Column(nullable = false)
-    private String role;
+    private UserRole role;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JsonManagedReference
